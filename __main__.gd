62b7b1dc99ed61dfbe5f79a78a60c83473bd9af5
Pskalou <pascal.padilla@gmail.com>
2020-02-26 00:53:25 +0100

    dépendances souples

--- __main__.gd
+++ __main__.gd
@@ -2,32 +2,14 @@ extends Node
 
 
 # touche ESC
-func _unhandled_input(event):
-    if event is InputEventKey:
-        if event.pressed and event.scancode == KEY_ESCAPE:
-            get_tree().quit()
 
-
-func _ready():	
-	var camera
-	camera = get_node("Camera2D")
-	camera._set_current(false)
+func _ready():
 	
-	var scene= get_node("MarginContainer/Node2D")
+	$GUI.show_popup()
 	
-	# Création d'une classe de 5 Students
-	var classroom = Classroom.new(50)
-	classroom.populate_test_classe(5, "2CAP_AEPE_")
-	
-	# Création d'une séance notée
-	var _seance= Seance.new(scene, classroom, 15)
-	
-	
-	# Affichage du pop-up
-	var texte = str(classroom)
-	
-	var info  = get_node("Popup/Info")
-	var popup = get_node("Popup")
-	popup.popup_centered_ratio()
-
-	info.set_text(texte)
+	# connexion GUI => World
+	$GUI.new_seance_from_buttons= funcref($World, "new_seance_from_GUI_buttons")
+	$GUI.new_seance_from_array	= funcref($World, "new_seance_from_GUI_array")
+	# connexion GUI <= World
+#	$GUI.score_text				= $World.score_text
+	$GUI.get_score	= funcref($World, "get_score")
